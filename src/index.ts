ee95a5942043580d7d8c456e57a924f34de735e6
Sasaya <storyn26383@gmail.com>
2024-06-25 00:52:18 +0800

    feat: cloudflare

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 import 'dotenv/config'
+import Cloudflare from 'cloudflare'
+
+const cloudflare = new Cloudflare({
+  apiToken: process.env.CLOUDFLARE_TOKEN,
+})
+
+const result = await cloudflare.user.tokens.verify()
+
+console.log(result)
